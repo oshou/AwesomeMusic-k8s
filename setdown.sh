29cc16e9d0506f6c4bfee02c9c17887cedd42dd4
oshou <adf1985adf@gmail.com>
2019-08-29 20:50:58 +0900

    add db-deployment & mackerel

--- setdown.sh
+++ setdown.sh
@@ -1,4 +1,6 @@
-kubectl delete -f ./namespace.yaml
 kubectl delete -f ./deployment-api.yaml
 kubectl delete -f ./deployment-front.yaml
 kubectl delete -f ./ingress.yaml
+kubectl delete -f ./daemonset-mackerel.yaml
+kubectl delete secret/regcred
+kubectl delete -f ./namespace.yaml
--- setup.sh
+++ setup.sh
@@ -1,6 +1,8 @@
+#kubectl config set-context gke_awesome-music-478f5_us-west1-c_myk8s --namespace=awesome-music
+#kubectl config use-context gke_awesome-music-478f5_us-west1-c_myk8s
 kubectl apply -f ./namespace.yaml
-kubectl config set-context minikube --namespace=awesome-music
 kubectl create secret docker-registry regcred --docker-server=docker.io --docker-username=oshou --docker-password=PBvifmQKM3h5f66 --docker-email=adf1985adf@gmail.com
 kubectl apply -f ./deployment-api.yaml
 kubectl apply -f ./deployment-front.yaml
 kubectl apply -f ./ingress.yaml
+kubectl apply -f ./daemonset-mackerel.yaml
