b2f4130b0d37dbf25c47c2a38dcad685801bf47e
oshou <adf1985adf@gmail.com>
2019-09-17 06:10:49 +0900

    環境変数渡し部分 修正

--- setdown.sh
+++ setdown.sh
@@ -1,5 +1,7 @@
 kubectl delete -f ./deployment-api.yaml
 kubectl delete -f ./deployment-front.yaml
+kubectl delete -f ./deployment-db.yaml
+kubectl delete -f ./deployment-debug.yaml
 kubectl delete -f ./ingress.yaml
 kubectl delete -f ./daemonset-mackerel.yaml
 kubectl delete secret/regcred
--- setup.sh
+++ setup.sh
@@ -1,17 +1,32 @@
-#kubectl config set-context gke_awesome-music-478f5_us-west1-c_myk8s --namespace=awesome-music
-#kubectl config use-context gke_awesome-music-478f5_us-west1-c_myk8s
+source ./.env
+
+# GKEのcontext等取得(ログイン認証完了済前提)
+# gcloud container clusters get-credentials $GKE_CLUSTER_NAME --zone $GKE_ZONE --project $GKE_PROJECT
+
+# コンテキスト指定
+kubectl config use-context minikube
+
+# Namespace指定
 kubectl apply -f ./namespace.yaml
+
+# 非公開情報登録
 kubectl create secret docker-registry regcred \
-  --docker-server=docker.io \
-  --docker-username=oshou \
-  --docker-password=PBvifmQKM3h5f66 \
-  --docker-email=adf1985adf@gmail.com
-kubectl create secret docker-registry regcred \
-  --docker-server=docker.io \
-  --docker-username=oshou \
-  --docker-password=PBvifmQKM3h5f66 \
-  --docker-email=adf1985adf@gmail.com
+  --docker-server=$DOCKER_SERVER \
+  --docker-username=$DOCKER_USERNAME \
+  --docker-password=$DOCKER_PASSWORD \
+  --docker-email=$DOCKER_EMAIL
+
+# DBコンテナ
+kubectl apply -f ./deployment-db.yaml
+
+# バックエンドAPI
 kubectl apply -f ./deployment-api.yaml
+
+# フロントSPA
 kubectl apply -f ./deployment-front.yaml
+
+# Ingress
 kubectl apply -f ./ingress.yaml
+
+# ノード監視(Mackerel)
 kubectl apply -f ./daemonset-mackerel.yaml
